7138f3cb039b16e393682a5239f58442f7b4cef5
DevTechResources <genericosistem@gmail.com>
2025-10-03 12:09:51 -0500

    Initial commit

---
+++ src/components/ContactPage.jsx
@@ -0,0 +1,11 @@
+import React, { useEffect } from 'react';
+
+const ContactPage = ({ onNavigate }) => {
+  useEffect(() => {
+    window.location.href = 'https://recursos-tecnologicos.com/contactos/';
+  }, []);
+
+  return null;
+};
+
+export default ContactPage;
