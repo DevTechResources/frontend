7174441dc43e7defa0dc6df0ae12129b55ef04ad
DevTechResources <genericosistem@gmail.com>
2025-10-07 12:32:36 -0500

    cambios

--- src/components/ImageProcessor.jsx
+++ src/components/ImageProcessor.jsx
@@ -17,7 +17,10 @@ const ImageProcessor = () => {
   const [error, setError] = useState('');
   const [switchProcessing, setSwitchProcessing] = useState(false);
   const fileInputRef = useRef(null);
-  const API_BASE_URL = 'https://coverage-earlier-backgrounds-clearing.trycloudflare.com/api';
+  const API_BASE_URL = import.meta.env.MODE === 'production'
+  ? 'https://coverage-earlier-backgrounds-clearing.trycloudflare.com/api'
+  : 'http://localhost:5000/api';
+ 
   
 
   const {
